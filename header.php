f040b2487265a37c2c2f1897eb1f85c6ced2b2f6
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2022-04-02 16:59:00 -0400

    fixed header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<h1>hMailServer IDS</h1>
 	</div>
 	<div class="headlinks">
-		<a href="./">Stats</a><?php if ($useGeoIP) {echo " | <a href='viewcountry.php'>By Country</a>";} ?> | <a href="viewall.php">All Data</a>
+		<a href="./">Stats</a><?php include_once("config.php"); if ($useGeoIP) {echo " | <a href='viewcountry.php'>By Country</a>";} ?> | <a href="viewall.php">All Data</a>
 	</div>
 
 </div>
